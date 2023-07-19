8b38feb918005b2cce81bbe203f9216511c2bbdb
RafayMemon01 <rafayrashid200611@gmail.com>
2023-07-19 15:00:50 +0500

    Not Working

---
+++ APIV1/main.mjs
@@ -0,0 +1,15 @@
+import express, { Router } from 'express';
+const router = express.Router();
+
+
+import authRouter from './routes/auth.mjs'
+import commentRouter from './routes/comment.mjs'
+import feedRouter from './routes/feed.mjs'
+import postRouter from './routes/post.mjs'
+
+router.use(authRouter)
+router.use( commentRouter)
+router.use( postRouter)
+router.use( feedRouter)
+
+export default router;
---
+++ server.mjs
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express()
+app.use(cors())
+
+import apiv1Router from './APIV1/main.mjs'
+import apiv2Router from './APIV2/main.mjs'
+
+
+
+app.use(express.json()); 
+
+app.use("/api/v1", apiv1Router)
+app.use("/api/v2", apiv2Router)
+
+
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+})
